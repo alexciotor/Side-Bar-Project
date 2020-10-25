48bd2679eead4feac3de22fd5068d1ecc3e77152
alexciotor <ciotor.alex@yahoo.com>
2020-10-25 10:47:30 +0200

    Side bar project

---
+++ main.js
@@ -0,0 +1,13 @@
+const logo = document.getElementById('Logo');
+const container = document.getElementById('Cont');
+const content = document.getElementById('Content')
+const whole =document.getElementById('whole')
+ 
+ 
+
+logo.addEventListener('click',function(){
+whole.classList.toggle('effect')
+
+})
+
+console.log(whole.classList)
